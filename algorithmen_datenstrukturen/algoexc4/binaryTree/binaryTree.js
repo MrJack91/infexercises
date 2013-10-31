c2de6032d60c210ab0b5ffd842f9adc6100dc71b
Michael Hadorn <michael.hadorn@gmail.com>
2013-10-31 08:29:16 +0100

    init tree

---
+++ algorithmen_datenstrukturen/algoexc4/binaryTree/binaryTree.js
@@ -0,0 +1,15 @@
+function subtree() {
+  this.leftSubtree = null;
+  this.rightSubtree = null;
+
+  this.data = null;
+}
+
+function treeManager() {
+  this.addNode = function () {
+
+  }
+
+}
+
+console.log('hi');
