4667ea31b3e333909daaf2d4958470f804383eb6
Dani <daniel.brun@gmx.net>
2013-11-02 14:24:30 +0100

    Algo - Binary-Tree

--- algorithmen_datenstrukturen/algoexc4/binaryTree/binaryTree.js
+++ algorithmen_datenstrukturen/algoexc4/binaryTree/binaryTree.js
@@ -6,10 +6,57 @@ function subtree() {
 }
 
 function treeManager() {
-  this.addNode = function () {
+  this.addNode = function (data) {
+		if(rootNode == null){
+			rootNode = new subtree();
+			rootNode.data = data;
+		}else{
+			preNode = null;
+			subTree = rootNode;
+			
+			while(subTree != null){
+				preNode = subTree;
+				if(data < subTree.data){
+					subTree = subTree.leftSubtree;
+				}else{
+					subTree = subTree.rightSubtree;
+				}
+			}
+			
+			node = new subtree();
+			node.data = data;
+			
+			if(data < preNode.data){
+				preNode.leftSubtree = node;
+			}else{
+				preNode.rightSubtree = node;
+			}
+			
+		}
+	}
+}
 
-  }
-
+function displayTreeNode(tree){
+	if(tree != null){
+		//TODO: Print value: tree.data
+		displayTreeNode(tree.leftSubtree);
+		displayTreeNode(tree.rightSubtree);
+	}
 }
 
 console.log('hi');
+
+var rootNode = null;
+var trMgm = null;
+
+trMgm = new treeManager();
+
+trMgm.addNode(5);
+trMgm.addNode(3);
+trMgm.addNode(8);
+console.log(rootNode);
+console.log(rootNode.leftSubtree);
+console.log(rootNode.rightSubtree);
+console.log(rootNode.data);
+
+displayTreeNode(rootNode);
