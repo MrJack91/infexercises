326dc98f0f711a905abda81a6d1275efbf82291e
Michael Hadorn <michael.hadorn@gmail.com>
2014-05-31 15:59:46 +0200

    add repeat function and debugging

--- informatik/lz77/Aufgabe5.js
+++ informatik/lz77/Aufgabe5.js
@@ -1,96 +1,171 @@
-var PREVIEW_BUFFER_SIZE = 4;
-var WORK_WINDOW_SIZE = 4;
 
-function compress(inputText) {
-  var workText = inputText;
-  var workWindow = "";
-  var previewBuffer = "";
 
-  var finishedCompression = false;
+var lz77 = {
 
-  //Remove!! only for debug!!
-  var count = 0;
+  PREVIEW_BUFFER_SIZE: 16,
 
-  console.log("Starting compression...");
-  while (!finishedCompression) {
-    console.log("...");
+  //TODO build workwindows size
+  WORK_WINDOW_SIZE: 16,
 
-    //Check if there is input text left
-    if (workText.length > 0) {
-      console.log("Process Text...");
-      //Calculate number of bits to transfer to previewBuffer
-      var prevBuffTransferCount = PREVIEW_BUFFER_SIZE - previewBuffer.length;
+  compress: function (inputText) {
+    var workText = inputText;
+    var workWindow = '';
+    var previewBuffer = '';
 
-      previewBuffer = previewBuffer + workText.substring(0, prevBuffTransferCount);
-      workText = workText.substring(prevBuffTransferCount, workText.length);
-      console.log("Buffer: " + previewBuffer);
-      console.log("Left Text: " + workText);
+    console.log('workText: ' + workText);
 
-      //Still work to do....
-      if (previewBuffer.length > 0) {
-        var foundMatch = false;
+    var finishedCompression = false;
 
-        var prevBufIndex = 0;
+    //Remove!! only for debug!!
+    var count = 0;
 
-        var lastPos = -1;
-        var lastLength = -1;
+    // console.log("Starting compression...");
+    while (!finishedCompression) {
+      console.log("...");
 
-        var currPos = -1;
-        var currLength = -1;
+      //Check if there is input text left
+      if (workText.length > 0 || previewBuffer.length > 0) {
+        // console.log("Process Text...");
+        //Calculate number of bits to transfer to previewBuffer
+        var prevBuffTransferCount = lz77.PREVIEW_BUFFER_SIZE - previewBuffer.length + 1;
 
-        //Iterate over workWindow
-        var iw = 0;
-        for (iw = 0; iw < workWindow.length; iw++) {
+        previewBuffer = previewBuffer + workText.substring(0, prevBuffTransferCount);
+        workText = workText.substring(prevBuffTransferCount, workText.length);
+        // console.log("Buffer: " + previewBuffer);
+        // console.log("Left Text: " + workText);
 
-          if (workWindow.charAt(iw) == previewBuffer.charAt(prevBufIndex)) {
-            currPos = iw;
-            currLength = 1;
-            //First character matches...
+        //Still work to do....
+        if (previewBuffer.length > 0) {
+          var foundMatch = false;
 
-            //Are there any more?
-            //Keep in boundary
-            var max = previewBuffer.length;
+          var prevBufIndex = 0;
 
-            if (workWindow.length < max) {
-              max = workWindow.length;
-            }
+          var lastPos = -1;
+          var lastLength = -1;
 
-            for (var x = 1; x < max; x++) {
-              if (workWindow.charAt(iw + x) == previewBuffer.charAt(prevBufIndex + x)) {
-                currLength++;
-              } else {
-                break;
+          var currPos = -1;
+          var currLength = -1;
+
+          //Iterate over workWindow
+          var iw = 0;
+          for (iw = 0; iw < workWindow.length; iw++) {
+            if (workWindow.charAt(iw) == previewBuffer.charAt(prevBufIndex)) {
+              foundMatch = true;
+
+              currPos = iw;
+              currLength = 1;
+              //First character matches...
+
+              //Are there any more?
+              //Keep in boundary
+              var max = previewBuffer.length;
+
+              if (workWindow.length < max) {
+                max = workWindow.length;
+              }
+
+              var x;
+              for (x = 1; x < max; x++) {
+                if (workWindow.charAt(iw + x) == previewBuffer.charAt(prevBufIndex + x)) {
+                  currLength++;
+                } else {
+                  break;
+                }
+              }
+
+              var newCurrLength = currLength;
+              // check if there any repeat in the previewbuffer (goes over the workwindow)
+              if (x == max) {
+                // search repeat of pattern (must be at the end of the workwindow)
+                for (var n = 0; n < previewBuffer.length; n++) {
+                  // compare workwindow and previewpuffer
+                  if (workWindow.charAt(currPos + (n%currLength)) == previewBuffer.charAt(n)) {
+                    newCurrLength++;
+                  } else {
+                    break;
+                  }
+                }
+              }
+
+              if (newCurrLength >= lastLength) {
+                lastLength = newCurrLength;
+                lastPos = currPos;
               }
             }
-
-            if (currLength >= lastLength) {
-              lastLength = currLength;
-              lastPos = currPos;
-            }
           }
-        }
 
-        if (foundMatch) {
-          //use lastLength + lastPos
-        } else {
-          //TODO: Add tripple: <0,0,previewBuffer.charAt(0)>
-          previewBuffer = previewBuffer.substring(1, previewBuffer.length);
+          console.log('lastLength: ' + lastLength);
+          console.log('lastPos: ' + lastPos);
+
+          console.log('previewBuffer: ' + previewBuffer);
+
+          if (foundMatch) {
+            //only for print
+            var pos = workWindow.length - lastPos;
+            var len = lastLength;
+
+            // handle overcut (repeat over workwindow)
+            var posNextChar;
+            if (len > pos) {
+              posNextChar = len-pos;
+            } else {
+              posNextChar = len;
+            }
+            var nextChar = previewBuffer.charAt(posNextChar);
+
+            // special case at the end, no next char available
+            if (nextChar.length == 0) {
+              nextChar = '-';
+            }
+
+            this.printTripel(pos, len, nextChar);
+
+            // add text to workwindow
+            workWindow += previewBuffer.substring(0, lastLength);
+
+            // recalc previewbuffer
+            previewBuffer = previewBuffer.substring(lastLength, previewBuffer.length);
+          } else {
+            this.printTripel(0, 0, previewBuffer.charAt(0));
+
+            // add text to workwindow
+            workWindow += previewBuffer.charAt(0);
+
+            previewBuffer = previewBuffer.substring(1, previewBuffer.length);
+          }
+
+
+          console.log('workWindow: ' + workWindow);
         }
+      } else {
+        // console.log("Finished compression...");
+        finishedCompression = true;
       }
-    } else {
-      console.log("Finished compression...");
-      finishedCompression = true;
+
+      count++;
+      if (count > 100) {
+        break;
+      }
     }
 
-    count++;
-    if (count > 20) {
-      break;
-    }
+    return null;
+  },
+
+  decompress: function (inputText) {
+
+  },
+
+
+  printTripel: function(pos, length, nextChar) {
+    console.log('<' + pos + ', ' + length + ', ' + nextChar + '>');
   }
 
-  return null;
-}
 
-function decompress(inputText) {
 
 }
+
+
+
+
+// lz77.compress('ACA');
+lz77.compress('AAAAC');
